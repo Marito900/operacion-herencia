2e960c1bc903c9a2b8b28603657793defa08cd72
Mario <Mari.duranp@duocuc.cl>
2026-09-22 23:17:57 -0300

    Primer commit: clase Dispositivo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Dispositivo dispositivo = new Dispositivo("Notebook hp", true);
+        dispositivo.mostrarEstado();
+        dispositivo.ejecutarDiagnostico();
+    }
+
+
+
+}
